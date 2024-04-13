31c6fdd69eeca9912742e0d9dc96b47ce9b34043
celsiusqc <63996164+celsiusqc@users.noreply.github.com>
2024-04-13 09:49:31 +0200

    common directory

--- common/src/main/java/net/celsiusqc/TowersOfTheWildModded.java
+++ common/src/main/java/net/celsiusqc/TowersOfTheWildModded.java
@@ -1,38 +1,9 @@
 package net.celsiusqc;
 
-import com.google.common.base.Suppliers;
-import dev.architectury.registry.CreativeTabRegistry;
-import dev.architectury.registry.registries.DeferredRegister;
-import dev.architectury.registry.registries.RegistrarManager;
-import dev.architectury.registry.registries.RegistrySupplier;
-import net.celsiusqc.ExampleExpectPlatform;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-
-import java.util.function.Supplier;
 
 public class TowersOfTheWildModded {
     public static final String MOD_ID = "totw_modded";
-    // We can use this if we don't want to use DeferredRegister
-    public static final Supplier<RegistrarManager> REGISTRIES = Suppliers.memoize(() -> RegistrarManager.get(MOD_ID));
 
-    // Registering a new creative tab
-    public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(MOD_ID, Registries.CREATIVE_MODE_TAB);
-    public static final RegistrySupplier<CreativeModeTab> EXAMPLE_TAB = TABS.register("example_tab", () ->
-            CreativeTabRegistry.create(Component.translatable("itemGroup." + MOD_ID + ".example_tab"),
-                    () -> new ItemStack(net.celsiusqc.TowersOfTheWildModded.EXAMPLE_ITEM.get())));
-    
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(MOD_ID, Registries.ITEM);
-    public static final RegistrySupplier<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () ->
-            new Item(new Item.Properties().arch$tab(net.celsiusqc.TowersOfTheWildModded.EXAMPLE_TAB)));
-    
     public static void init() {
-        TABS.register();
-        ITEMS.register();
-        
-        System.out.println(ExampleExpectPlatform.getConfigDirectory().toAbsolutePath().normalize().toString());
     }
 }
